41b490c7916e63819638a397ae6a8da6cd932d4a
John Pablo <john.davis.2015.03@gmail.com>
2023-01-21 15:31:35 -0500

    modificamos variables de entorno

--- app/schemas/product.schema.js
+++ app/schemas/product.schema.js
@@ -10,12 +10,15 @@ const name = Joi.string()
       'The product name accepts alphabetic characters, numbers and spaces',
   });
 const image = Joi.string().uri();
-const price = Joi.number().integer().min(10);
 const description = Joi.string().min(10);
 const categoryId = Joi.number().integer();
+const price = Joi.number().integer().min(10);
 
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
+
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
 
 const getProductSchema = Joi.object({
   id: id.required(),
@@ -41,7 +44,15 @@ const queryProductSchema = Joi.object({
   limit,
   offset,
   price,
-});
+  price_min,
+  price_max: price_max.greater(Joi.ref('price_min')),
+  // price_max: price_max.when('price_min', {
+  //   is: Joi.number().integer(),
+  //   then: Joi.required(),
+  // }),
+})
+  .with('price_min', 'price_max')
+  .with('price_max', 'price_min');
 
 module.exports = {
   getProductSchema,
