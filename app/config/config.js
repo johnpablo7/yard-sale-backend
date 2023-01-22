415ff72666c52955618b5d311d9ffdd3f8217e25
John Pablo <john.davis.2015.03@gmail.com>
2023-01-21 22:28:02 -0500

    Use APP_ENV instead of NODE_ENV

--- app/config/config.js
+++ app/config/config.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
 const config = {
-  env: process.env.NODE_ENV || 'development',
-  isProd: process.env.NODE_ENV === 'production',
+  env: process.env.APP_ENV || 'development',
+  isProd: process.env.APP_ENV === 'production',
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
+  // dbUser: process.env.DB_USER,
+  // dbPassword: process.env.DB_PASSWORD,
+  // dbHost: process.env.DB_HOST,
+  // dbName: process.env.DB_NAME,
+  // dbPort: process.env.DB_PORT,
   dbUrl: process.env.DATABASE_URL,
 };
 
